7092350b89af08b2ca6b0b382b484aeb7e395103
Roger Araujo <rogeralves_90@yahoo.com.br>
2020-03-26 22:35:59 -0300

    frontend code

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+import heroesImg from '../../assets/heroes.png';
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form"></section>
+            <img src={heroesImg} alt="Heroes"></img>
+        </div>
+    )
+}
